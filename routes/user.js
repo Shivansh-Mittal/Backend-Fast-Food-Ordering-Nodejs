81659d2bbe207ce3c68bbce437f16dcc14770495
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-12-26 22:47:20 +0530

    added code in user api

--- routes/user.js
+++ routes/user.js
@@ -6,51 +6,53 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // signup api
-router.post('/signup', (req,res) => {
+router.post('/signup', (req, res) => {
     let user = req.body;
     query = "select email,password,role,status from user where email=?";
-    connection.query(query, [user.email], (err,results) => {
-        if(!err){
-            if(results.length <= 0){
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
                 query = "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
-                connection.query(query, [user.name, user.contactNumber, user.email, user.password], (err,results) => {
-                    if(!err){
-                        return res.status(200).json({message: "Successfully Registered"});
-                    }else{
+                connection.query(query, [user.name, user.contactNumber, user.email, user.password], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json({ message: "Successfully Registered" });
+                    } else {
                         return res.status(500).json(err);
                     }
                 })
             }
-            else{
-                return res.status(400).json({message: "Email is already registered. Please Signin with your account."});
+            else {
+                return res.status(400).json({ message: "Email is already registered. Please Signin with your account." });
             }
         }
-        else{
+        else {
             return res.status(500).json(err);
         }
     })
 })
 
 // login api
-router.post('/login', (req,res) => {
+router.post('/login', (req, res) => {
     const user = req.body;
     query = "select email,password,role,status from user where email=?";
-    connection.query(query, [user.email], (err,results) => {
-        if(!err){
-            if(results.length <= 0 || results[0].password != user.password){
-                return res.status(401).json({message:"Invalid Credentials"});
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: "Invalid Credentials" });
             }
-            else if(results[0].status == 'false'){
-                return res.status(401).json({message:"Wait for Admin Approval"});
+            else if (results[0].status == 'false') {
+                return res.status(401).json({ message: "Wait for Admin Approval" });
             }
-            else if(results[0].password == user.password){
-
+            else if (results[0].password == user.password) {
+                const response = { email: results[0].email, role: results[0].role };
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+                res.status(200).json({ token: accessToken });
             }
-            else{
-                return res.status(400).json({message:"Something went wrong. Please try again later"});
+            else {
+                return res.status(400).json({ message: "Something went wrong. Please try again later" });
             }
         }
-        else{
+        else {
             return res.status(500).json(err);
         }
     })
